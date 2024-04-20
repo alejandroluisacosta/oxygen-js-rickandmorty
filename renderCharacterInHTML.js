9cdea70955d84bb633c420319b54d3e1e9447241
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-20 16:52:14 +0200

    add: characters rendered as grid

--- renderCharacterInHTML.js
+++ renderCharacterInHTML.js
@@ -1,14 +1,34 @@
 
 const grid = document.getElementById("characters-grid");
 
-const fetchCharactersLocation = async character => {
-    
+const fetchCharactersEpisode = async episodeURL => {
+    try {
+        const response = await fetch(episodeURL[0]);
+        if (response.ok) {
+            try {
+                const jsonData = await response.json();
+                console.log(jsonData.name);
+                return jsonData.name;
+            }
+            catch(error) {
+                console.log(`Error ${error}`);
+            }
+        }
+    }
+    catch(error) {
+        console.log(`Error: ${error}`);
+    }
 }
 
 const renderCharacterInHTML = (characterArray) => {
     characterArray.forEach(character => {
         const article = document.createElement("article");
         grid.appendChild(article);
+
+        const image = document.createElement("img");
+        image.src = character.image;
+
+        const infoDiv = document.createElement("div");
 
         const name = document.createElement("p");
         name.innerText = character.name;
@@ -20,7 +40,7 @@ const renderCharacterInHTML = (characterArray) => {
         const locationTitle = document.createElement("p");
         locationTitle.innerText = "Last known location:";
         const locationValue = document.createElement("p");
-        locationValue.innerText = `${character.location.name}`;
+        locationValue.innerText = (character.location.name);
         locationContainer.appendChild(locationTitle);
         locationContainer.appendChild(locationValue);
 
@@ -28,14 +48,16 @@ const renderCharacterInHTML = (characterArray) => {
         const episodeTitle = document.createElement("p");
         episodeTitle.innerText = "First seen in:";
         const episodeValue = document.createElement("p");
-        episodeValue.innerText = `${character.episode}`;
+        episodeValue.innerText = fetchCharactersEpisode(character.episode);
         episodeContainer.appendChild(episodeTitle);
         episodeContainer.appendChild(episodeValue);
 
-        article.appendChild(name);
-        article.appendChild(description);
-        article.appendChild(locationContainer);
-        article.appendChild(episodeContainer);
+        article.appendChild(image);
+        article.appendChild(infoDiv);
+        infoDiv.appendChild(name);
+        infoDiv.appendChild(description);
+        infoDiv.appendChild(locationContainer);
+        infoDiv.appendChild(episodeContainer);
 
     })
 }
