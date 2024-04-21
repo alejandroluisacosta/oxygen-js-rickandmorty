358612ee63ada7cbaf341b8d343f9ac46b8fcf5f
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-21 15:16:14 +0200

    add: sass files and first styles

--- renderCharacterInHTML.js
+++ renderCharacterInHTML.js
@@ -1,5 +1,5 @@
 
-const grid = document.getElementById("characters-grid");
+const grid = document.getElementById("grid");
 
 const fetchCharactersEpisode = async episodeURL => {
     try {
@@ -23,34 +23,59 @@ const fetchCharactersEpisode = async episodeURL => {
 const renderCharacterInHTML = (characterArray) => {
     characterArray.forEach(character => {
         const article = document.createElement("article");
+        article.classList.add('grid__article');
         grid.appendChild(article);
 
         const image = document.createElement("img");
+        image.classList.add('grid__article__image');
         image.src = character.image;
 
         const infoDiv = document.createElement("div");
+        infoDiv.classList.add('grid__article__info');
 
         const name = document.createElement("p");
         name.innerText = character.name;
+        name.classList.add('grid__article__info__name');
         
+        
+
         const description = document.createElement("p");
+        description.classList.add('grid__article__info__description');
         description.innerText = `${character.status} - ${character.species}`;
 
+
+
         const locationContainer = document.createElement("div");
+        locationContainer.classList.add('grid__article__info__location');
+        
         const locationTitle = document.createElement("p");
+        locationTitle.classList.add('grid__article__info__location__title');
         locationTitle.innerText = "Last known location:";
+        
         const locationValue = document.createElement("p");
+        locationValue.classList.add('grid__article__info__location__value');
         locationValue.innerText = (character.location.name);
+        
         locationContainer.appendChild(locationTitle);
         locationContainer.appendChild(locationValue);
 
+
+
         const episodeContainer = document.createElement("div");
+        episodeContainer.classList.add('grid__article__info__episode');
+        
         const episodeTitle = document.createElement("p");
+        episodeTitle.classList.add('grid__article__info__episode__title');
         episodeTitle.innerText = "First seen in:";
+
         const episodeValue = document.createElement("p");
+        episodeValue.classList.add('grid__article__info__episode__value');
         episodeValue.innerText = fetchCharactersEpisode(character.episode);
+        
         episodeContainer.appendChild(episodeTitle);
         episodeContainer.appendChild(episodeValue);
+
+
 
         article.appendChild(image);
         article.appendChild(infoDiv);
